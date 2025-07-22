c0f476528b58c3fb88e1c9a64e3d6d5635777d28
Andrik88 <fatschnitzel@gmail.com>
2025-07-22 09:29:07 +0300

    Added Player vehicle, explosions and simple colliders

---
+++ Assets/Scripts/MineControl.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MineControl : MonoBehaviour
+{   
+    public GameObject explosionPrefab;
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Mine says: I'm still colliding with the ground" + other.name);
+        if (other.GetComponent<CarControl>() != null)
+        {
+            Debug.Log("Mine says: YOU'RE DEAD " + other.name);
+            Explode();
+        }
+    }
+    private void Explode()
+    {
+        //Debug.Log("Mine says: I should've been exploded");
+        if (explosionPrefab != null)
+        {
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+}
