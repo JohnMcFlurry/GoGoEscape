750035a55797d91a6c8c9f57529ecf67e7974533
Andrik88 <fatschnitzel@gmail.com>
2025-07-23 23:03:23 +0300

    Added cameras and camera switch

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,46 @@
+using Unity.Cinemachine;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera[] cameras;
+
+    private int currentCamera = 0;
+
+    private int activePriority = 10;
+    private int inactivePriority = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            currentCamera++;
+            if (currentCamera >= cameras.Length)
+            {
+                currentCamera = 0;
+            }
+            UpdateCamera();
+        }
+    }
+    private void UpdateCamera()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            CinemachineCamera cam = cameras[i];
+            if (i == currentCamera)
+            {
+                cam.Priority = activePriority;
+            }
+            else
+            {
+                cam.Priority = inactivePriority;
+            }
+        }
+    }
+}
